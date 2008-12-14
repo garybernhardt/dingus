c07277aff75146466c138328acef017f8f6aac1e
Gary Bernhardt <gary.bernhardt@gmail.com>
2008-12-14 18:13:27 -0500

    Added a real setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,17 @@ from setuptools import setup, find_packages
 
 setup(name='dingus',
       version='0.1',
-      description='Dingus',
+      description='A record-then-assert mocking library',
       author='Gary Bernhardt',
       author_email='gary.bernhardt@gmail.com',
       packages=find_packages(),
-      license='BSD',
+      license='MIT',
+      keywords='testing test mocking mock double stub fake'
+      classifiers=["Development Status :: 2 - Pre-Alpha",
+                   "Intended Audience :: Developers",
+                   "License :: OSI Approved :: MIT License",
+                   "Operating System :: OS Independent",
+                   "Programming Language :: Python",
+                   "Topic :: Software Development :: Testing",
+                   ],
      )
