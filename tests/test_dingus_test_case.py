57441479d854eeb52b2b64d0e5e0457f621244c6
Gary Bernhardt <gary.bernhardt@gmail.com>
2009-03-29 22:34:47 -0400

    Simplified DingusTestCase tests

--- tests/test_dingus_test_case.py
+++ tests/test_dingus_test_case.py
@@ -1,65 +1,40 @@
 import sys
 import new
+from tests import test_case_fixture as module
+from tests.test_case_fixture import ClassUnderTest
 
 from dingus import DingusTestCase, Dingus
 
 
-class WhenRunningTestCaseOnAModule(object):
+class WhenCallingSetupFunction:
     def setup(self):
-        self.module = self._create_fake_module()
-        self.class_under_test = self._create_class_under_test()
-        self.test_case_class = self._create_test_case_class()
-
-    def _create_fake_module(self):
-        module = new.module('test_module')
-        module.value = 'value'
-        sys.modules[module.__name__] = module
-        return module
-
-    def _create_class_under_test(self):
-        class ClassThatWouldBeUnderTest:
+        class TestCase(DingusTestCase(module.ClassUnderTest)):
             pass
-        ClassThatWouldBeUnderTest.__module__ = self.module.__name__
-        self.module.ClassThatWouldBeUnderTest = ClassThatWouldBeUnderTest
-        return ClassThatWouldBeUnderTest
-
-    def _create_test_case_class(self):
-        class TestCaseClass(DingusTestCase(self.class_under_test)):
-            pass
-        return TestCaseClass
-
-    def teardown(self):
-        del sys.modules[self.module.__name__]
-
-
-class WhenCallingSetupFunction(WhenRunningTestCaseOnAModule):
-    def setup(self):
-        super(WhenCallingSetupFunction, self).setup()
-        self.test_case_instance = self.test_case_class()
+        self.test_case_instance = TestCase()
         self.test_case_instance.setup()
 
     def teardown(self):
         self.test_case_instance.teardown()
-        super(WhenCallingSetupFunction, self).teardown()
 
     def should_replace_module_attributes(self):
-        assert isinstance(self.module.value, Dingus)
+        assert isinstance(module.atomic_value, Dingus)
 
     def should_leave_class_under_test_intact(self):
-        assert self.module.ClassThatWouldBeUnderTest is self.class_under_test
+        assert module.ClassUnderTest is ClassUnderTest
 
 
-class WhenCallingTeardownFunction(WhenRunningTestCaseOnAModule):
+class WhenCallingTeardownFunction:
     def setup(self):
-        super(WhenCallingTeardownFunction, self).setup()
-        self.test_case_object = self.test_case_class()
-        self.original_module_dict = self.module.__dict__.copy()
-        self.test_case_object.setup()
-        self.test_case_object.teardown()
+        self.original_module_dict = module.__dict__.copy()
+        class TestCase(DingusTestCase(module.ClassUnderTest)):
+            pass
+        test_case_object = TestCase()
+        test_case_object.setup()
+        test_case_object.teardown()
 
     def should_restore_module_attributes(self):
-        assert self.module.value is 'value'
+        assert module.atomic_value is 'foo'
 
     def should_leave_globals_as_they_were_before_dingusing(self):
-        assert self.module.__dict__ == self.original_module_dict
+        assert module.__dict__ == self.original_module_dict
 
