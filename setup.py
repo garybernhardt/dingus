cf1520c7e46890115b1cac77ac571cfcd80b2c2f
Gary Bernhardt <gary.bernhardt@gmail.com>
2011-02-24 11:30:51 -0800

    update version to 0.3.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='dingus',
-      version='0.3.1',
+      version='0.3.2',
       description='A record-then-assert mocking library',
       long_description=file('README.txt').read(),
       author='Gary Bernhardt',
