f98355989569e3279bbb997e2b5e8d374a34bf35
Paulo Poiati <paulogpoiati@gmail.com>
2012-09-17 23:55:39 -0300

    moved the assert_call method to the Dingus class

--- tests/test_assert_call.py
+++ tests/test_assert_call.py
@@ -1,4 +1,4 @@
-from dingus import assert_call, Dingus
+from dingus import Dingus
 from nose.tools import raises
 
 
@@ -12,49 +12,46 @@ class WhenCallsExists(AssertCallTest):
     def should_not_raise_any_error_simple_call(self):
         self.ding.foo()
 
-        assert_call(self.ding, 'foo')
+        self.ding.foo.assert_call()
 
     def should_not_raise_any_error_with_args(self):
         self.ding.foo('bar')
 
-        assert_call(self.ding, 'foo')
-        assert_call(self.ding, 'foo', 'bar')
-        
+        self.ding.foo.assert_call()
+        self.ding.foo.assert_call('bar')
+
     def should_not_raise_any_error_with_args_and_kwargs(self):
         self.ding.foo('bar', qux=1)
 
-        assert_call(self.ding, 'foo')
-        assert_call(self.ding, 'foo', 'bar')
-        assert_call(self.ding, 'foo', 'bar', qux=1)
+        self.ding.foo.assert_call()
+        self.ding.foo.assert_call('bar')
+        self.ding.foo.assert_call('bar', qux=1)
 
 class WhenThereIsNoCallsForTheMatchedArgs(AssertCallTest):
 
     @raises(AssertionError)
     def should_raise_an_assertion_error(self):
-        assert_call(self.ding, 'foo')
-        
+        self.ding.foo.assert_call()
+
     @raises(AssertionError)
     def should_raise_an_assertion_error_other_method_call(self):
         self.ding.bar()
-        assert_call(self.ding, 'foo')
+
+        self.ding.foo.assert_call()
 
     @raises(AssertionError)
     def should_raise_an_assertion_error_with_args(self):
         self.ding.foo()
 
-        assert_call(self.ding, 'foo', 'bar')
+        self.ding.foo.assert_call('bar')
 
     @raises(AssertionError)
     def should_raise_an_assertion_error_with_args_and_kargs(self):
         self.ding.foo('bar')
 
-        assert_call(self.ding, 'foo', 'bar', qux=1)
+        self.ding.foo.assert_call('bar', qux=1)
 
     def should_show_a_friendly_error_message(self):
-        self._test_expectation_message('foo')
-
-    def should_show_a_friendly_error_message_with_call_to_other_method(self):
-        self.ding.bar()
         self._test_expectation_message('foo')
 
     def should_show_a_friendly_error_message_with_args(self):
@@ -65,17 +62,17 @@ class WhenThereIsNoCallsForTheMatchedArgs(AssertCallTest):
 
     def _test_expectation_message(self, method, *args, **kwargs):
         try:
-            assert_call(self.ding, method, *args, **kwargs)
+            dingus = getattr(self.ding, method)
+            dingus.assert_call(*args, **kwargs)
         except AssertionError, e:
-            self._assert_message(e.message, method, args, kwargs)
+            self._assert_message(e.message, dingus, args, kwargs)
         else:
             assert False, 'should not be here'
 
-    def _assert_message(self, message, method, args, kwargs):
+    def _assert_message(self, message, dingus, args, kwargs):
             expected, recorded_calls = message.split('\n')
 
-            assert "Expected a call to method: '%s', args: %s, kwargs: %s, " % (method, args, kwargs)  + \
-                   "dingus: %s" % self.ding == expected
+            assert "Expected a call to: '%s', args: %s, kwargs: %s, " % (dingus, args, kwargs)
 
             if not self.ding.calls:
                 assert "No calls recorded" == recorded_calls
